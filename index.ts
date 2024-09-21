53f3814d3d5837cc92b1f86ece3519c429c99432
Edwin Ortiz <edarcode@gmail.com>
2024-09-21 18:45:43 -0500

    creando una mini api con bun

--- index.ts
+++ index.ts
@@ -1,11 +1,40 @@
-import figlet from "figlet";
+import { PRODUCTS } from "./src/db/products";
 
 const server = Bun.serve({
-  port: 3000,
-  fetch(_req) {
-    const body = figlet.textSync("Bun!");
-    return new Response(body);
+  port: process.env.PORT,
+  fetch(req) {
+    const url = new URL(req.url);
+    const pathname = url.pathname;
+    const { method } = req;
+
+    console.log(method, url.origin);
+
+    if (pathname === "/" && method === "GET") {
+      const json = JSON.stringify({ msg: "¡Bienvenido!" });
+      return new Response(json, {
+        status: 200,
+        headers: { "Content-Type": "application/json" },
+      });
+    }
+
+    if (pathname === "/products" && method === "GET") {
+      const json = JSON.stringify(PRODUCTS);
+      return new Response(json, {
+        status: 200,
+        headers: { "Content-Type": "application/json" },
+      });
+    }
+
+    return new Response(
+      JSON.stringify({
+        msg: "Mani te estás tirando un triple, revisa la ruta.",
+      }),
+      {
+        status: 404,
+        headers: { "Content-Type": "application/json" },
+      }
+    );
   },
 });
 
-console.log(`Listening on http://localhost:${server.port} ...`);
+console.log(`Listening on http://localhost:${server.port}`);
---
+++ src/db/products.ts
@@ -0,0 +1,5 @@
+export const PRODUCTS = [
+  { id: "1", name: "arepa", price: "7000" },
+  { id: "2", name: "gaseosa", price: "4500" },
+  { id: "3", name: "empanada", price: "1000" },
+];
